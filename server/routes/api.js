c1424d76d1d97f33d51ce62dfeed87905f22c3d3
Ian <ian.k.carson@gmail.com>
2020-08-30 21:12:34 -0400

    adding more routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,12 +2,12 @@ const router = require("express").Router()
 const path = require('path')
 const { Champion, ChampInfo} = require('../db')
 
-router.get('/', async (req, res, next)=>{
+router.get('/champions', async (req, res, next)=>{
     const data = await Champion.findAll();
     res.send(data);
 })
 
-router.get('/champions/$id', async (req, res, next)=>{
+router.get('/champions/:id', async (req, res, next)=>{
     const data = await Champion.find({where:{id:req.params.id}});
     res.send(data);
 })
